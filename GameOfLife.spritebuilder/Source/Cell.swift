c9f781cbe8fa2b3eccf7ecb55a6f11ac80fa83c1
gchablowski <g.chablowski@free.fr>
2015-09-04 14:52:36 +0200

    Create class grid and cell

---
+++ GameOfLife.spritebuilder/Source/Cell.swift
@@ -0,0 +1,14 @@
+//
+//  Cell.swift
+//  GameOfLife
+//
+//  Created by gérald chablowski on 04/09/2015.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Cocoa
+
+class Cell: CCSprite {
+    
+
+}
