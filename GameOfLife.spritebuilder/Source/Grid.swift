341864e6289f1179b9d943f935bfe8d1eba9cc6e
gchablowski <g.chablowski@free.fr>
2015-09-05 14:08:33 +0200

    refactor setupGrid function

--- GameOfLife.spritebuilder/Source/Grid.swift
+++ GameOfLife.spritebuilder/Source/Grid.swift
@@ -27,11 +27,14 @@ class Grid: CCSprite {
     
     func setupGrid() {
         
+        //create a number of cell randomly
         totalAlive = Int(arc4random_uniform(3000))
         
+        //get the size of the grid
         sizeX = Int(contentSizeInPoints.width)
         sizeY = Int(contentSizeInPoints.height)
         
+        //create the cell
         for i in 0..<totalAlive {
             var randX = Int(arc4random_uniform(UInt32(sizeX)))
             var randY = Int(arc4random_uniform(UInt32(sizeY)))
@@ -43,57 +46,8 @@ class Grid: CCSprite {
             
             //add cell to the view
             addChild(cell)
-        
+            //add cell to our dictionary with a hash as key
             gridDictionary["\(randX)-\(randY)"] = cell
-            //save the 8 other cell for the futur generation
-           for j in 0..<8 {
-                var CoordX = 0
-                var CoordY = 0
-                var cellDisable = Cell()
-
-                switch (j)
-                {
-                case 0:
-                    CoordX = randX-1
-                    CoordY = randY-1
-                    
-                case 1:
-                    CoordX = randX
-                    CoordY = randY-1
-                    
-                case 2:
-                    CoordX = randX+1
-                    CoordY = randY-1
-                    
-                case 3:
-                    CoordX = randX-1
-                    CoordY = randY
-                    
-                case 4:
-                    CoordX = randX+1
-                    CoordY = randY
-                    
-                case 5:
-                    CoordX = randX
-                    CoordY = randY+1
-                    
-                case 6:
-                    CoordX = randX-1
-                    CoordY = randY+1
-                    
-                default:
-                    CoordX = randX+1
-                    CoordY = randY+1
-
-                }
-
-                
-                cellDisable.x = CGFloat(CoordX)
-                cellDisable.y = CGFloat(CoordY)
-                cellDisable.visible = false
-                
-                gridDictionary["\(CoordX)-\(CoordY)"] = cellDisable
-            }
             
         }
     }
